e9bc033508d0f0e3231804d2e91a49d0f226fb37
Shivam Mittal <shivammittal.mittal.10@gmail.com>
2024-08-29 16:43:11 +0530

    Inital Project Setup

---
+++ src/components/index.js
@@ -0,0 +1,32 @@
+import Select from './Select'
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./Container/container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from './Logo';
+import RTE from './RTE';
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from './AuthLayout'
+import Input from './input'
+import Button from './Button'
+
+
+export {
+    Select,
+    Header,
+    Footer,
+    Container,
+    LogoutBtn,
+    Logo,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Input,
+    Button
+}
